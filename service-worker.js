41638c892990ba235c834a48235e63cdec8ada07
curtdunford <curtdunford@shaw.ca>
2025-11-15 16:02:07 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const cacheName = 'cashcount-v1';
+const assets = [
+  '/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
